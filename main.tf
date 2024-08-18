487324b7637dec13e95c23b85f6ffbd06e963183
Jordan G <30913872+jordangoodfellow@users.noreply.github.com>
2024-08-18 16:36:10 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,37 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.102.0" # Specify the desired version
+    }
+ aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    } 
+  
+  }
+
+  backend "s3" {
+    bucket         	   = "goodfellow-tech-dev"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "eu-central-1"
+    encrypt        	   = true
+    dynamodb_table = "mycomponents_tf_lockid"
+  }
+}
+
+# Provides configfuration details for Azure Terraform provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "test-1"
+  location = "East Us"
+  tags = {
+    environment = "dev"
+  }
+}
+
